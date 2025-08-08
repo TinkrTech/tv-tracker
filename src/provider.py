985f5dc233479ef69a0bc7142ea5dee057b59015
Jade T <jade@tinkrtech.net>
2025-08-08 16:09:15 -0400

    Add series fetching

--- src/provider.py
+++ src/provider.py
@@ -3,40 +3,78 @@ Note: This uses unsanitized inputs for routing. Use caution when loading config.
 """
 import requests
 import urllib.parse
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 import textwrap
+from datetime import date
 
+@dataclass(slots=True, frozen=True)
+class Season:
+    tvdb_id: int
+    number: int
+    order: str
 
 @dataclass(slots=True, frozen=True)
 class Series:
-    tvdb_id: str
+    tvdb_id: int
     title: str
+    year: str
+    last_aired: date
+    retrieved: date
+    keep_updated: bool
+    use_order: str
+    orders: list[str]
+    seasons: list[Season] = field(default_factory=list)
+    _num_seasons: Optional[int] = None
+
+    def __str__(self) -> str:
+        return textwrap.dedent(f"""\
+            ["{self.title} ({self.year})"]
+            tvdb_id = {self.tvdb_id}
+            title = "{self.title}"
+            year = "{self.year}"
+            seasons = {self.season_count}
+            last_aired = "{self.last_aired}"
+            retrieved = "{self.retrieved}"
+            keep_updated = {str(self.keep_updated).lower()}
+            orders = {self.orders}
+            use_order = "{self.use_order}"
+        """).strip()
+
+    @property
+    def season_count(self) -> int:
+        if self._num_seasons is None:
+            return len([season for season in self.seasons if season.number != 0 and season.order == self.use_order])
+        else:
+            return self._num_seasons
+
+
+@dataclass(slots=True, frozen=True)
+class SearchResponse:
+    tvdb_id: int
+    title: str
+    year: str
     language: str
-    release_year: str
     synopsis: str
-    status: str
-    thumbnail: Optional[str]
 
     def __str__(self):
-        return textwrap.dedent(f"""
+        return textwrap.dedent(f"""\
             id: {self.tvdb_id}
             title: {self.title}
-            year: {self.release_year}
+            year: {self.year}
+            language: {self.language}
             synopsis: {self.synopsis}
-        """.strip('\n'))
+        """).strip()
 
     @classmethod
-    def from_dict(cls, data: dict):
+    def from_dict(cls, data: dict) -> Self:
         thumbnail = data.get('thumbnail', data.get('image_url', None))
-        return Series(
+        return cls(
             tvdb_id=data['tvdb_id'],
             title=data['name'],
             language=data['primary_language'],
-            release_year=data['year'],
+            year=data['year'],
             synopsis=data['overview'],
-            thumbnail=thumbnail,
-            status=data['status'],
         )
 
 API_URL = "https://api4.thetvdb.com/v4"
@@ -63,11 +101,39 @@ def _make_request(session_token: str, endpoint: str, *, query: dict = None) -> d
     response.raise_for_status()
     return response.json()
 
-def search(session_token: str, query: dict) -> list[Series]:
+def search(session_token: str, query: dict) -> list[SearchResponse]:
     filtered_query = {key: value for key, value in query.items() if value is not None}
     raw = _make_request(session_token, 'search', query=filtered_query)
-    return [Series.from_dict(result) for result in raw['data']]
+    return [SearchResponse.from_dict(result) for result in raw['data']]
 
-def series_info(session_token: str, tvdb_id: str):
-    raw = _make_request(session_token, endpoint=f"series/{tvdb_id}/extended")
-    return raw["data"]
+def get_series_info(session_token: str, tvdb_id: int, use_order: str = None) -> Series:
+    raw = _make_request(session_token, endpoint=f"series/{tvdb_id}/extended")["data"]
+
+    keep_updated = raw\
+        .get('status', {})\
+        .get("keepUpdated", True)
+
+    orders = [order["name"] for order in raw["seasonTypes"]]
+    if use_order is None:
+        use_order = orders[0]
+
+    seasons = []
+    for raw_season in raw.get("seasons"):
+        season = Season(
+            tvdb_id=raw_season["id"],
+            number=raw_season['number'],
+            order=raw_season["type"]["name"]
+        )
+        seasons.append(season)
+
+    return Series(
+        tvdb_id=tvdb_id,
+        title = raw["name"],
+        year = raw["year"],
+        last_aired = date.fromisoformat(raw["lastAired"]),
+        retrieved = date.today(),
+        keep_updated=keep_updated,
+        use_order=use_order,
+        orders=orders,
+        seasons=seasons,
+    )

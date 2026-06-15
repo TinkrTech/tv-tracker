2f36c1a702a2f2075243f81418b96429cb753622
Jade T <jade@tinkrtech.net>
2026-06-15 13:30:31 -0400

    tests: Add cases for provider.fetch_series

--- tests/common/test_provider.py
+++ tests/common/test_provider.py
@@ -3,7 +3,10 @@
 # Author: Jade T <jade@tinkrtech.net>
 import pytest
 from pytest import MonkeyPatch
+
+import datetime
 from common.provider import TVDBProvider, SearchResult
+from common import model
 from common import cache
 
 @pytest.fixture
@@ -74,6 +77,171 @@ def mock_search_response() -> dict:
         ],
     }
 
+def mock_series_response() -> dict:
+    return {
+        "status": "success",
+        "data": {
+            "id": 78874,
+            "name": "Firefly",
+            "slug": "firefly",
+            "nameTranslations": ["deu","eng","fra","swe",],
+            "overviewTranslations": ["deu","eng","fra","swe",],
+            "firstAired": "2002-09-20",
+            "lastAired": "2003-07-28",
+            "nextAired": "",
+            "status": {
+                "id": 2,
+                "name": "Ended",
+                "recordType": "series",
+                "keepUpdated": False
+            },
+            "originalCountry": "usa",
+            "originalLanguage": "eng",
+            "defaultSeasonType": 1,
+            "isOrderRandomized": False,
+            "lastUpdated": "2026-05-17 13:24:13",
+            "averageRuntime": 48,
+            "episodes": None,
+            "overview": "In the far-distant future, Captain Malcolm \"Mal\" Reynolds is a renegade former brown-coat sergeant, now turned smuggler & rogue, who is the commander of a small spacecraft, with a loyal hand-picked crew made up of the first mate, Zoe Warren; the pilot Hoban \"Wash\" Washburn; the gung-ho grunt Jayne Cobb; the engineer Kaylee Frye; the fugitives Dr. Simon Tam and his psychic sister River. Together, they travel the far reaches of space in search of food, money, and anything to live on.",
+            "year": "2002",
+            "seasons": [
+            {
+                "id": 15791,
+                "seriesId": 78874,
+                "type": {
+                    "id": 1,
+                    "name": "Aired Order",
+                    "type": "official",
+                    "alternateName": "Aired Order"
+                },
+                "number": 1,
+                "nameTranslations": [],
+                "overviewTranslations": ["eng,fra"],
+                "lastUpdated": "2026-03-02 18:42:16"
+            },
+            {
+                "id": 1775413,
+                "seriesId": 78874,
+                "type": {
+                "id": 3,
+                    "name": "Absolute Order",
+                    "type": "absolute",
+                    "alternateName": None
+                },
+                "number": 1,
+                "nameTranslations": [],
+                "overviewTranslations": [],
+                "lastUpdated": "2026-03-02 18:42:16"
+            },
+            {
+                "id": 1775414,
+                "seriesId": 78874,
+                "type": {
+                    "id": 2,
+                    "name": "DVD Order",
+                    "type": "dvd",
+                    "alternateName": None
+                },
+                "number": 1,
+                "nameTranslations": [],
+                "overviewTranslations": [],
+                "lastUpdated": "2026-03-02 18:42:16"
+            },
+            {
+                "id": 1887664,
+                "seriesId": 78874,
+                "type": {
+                "id": 2,
+                    "name": "DVD Order",
+                    "type": "dvd",
+                    "alternateName": None
+                },
+                "number": 0,
+                "nameTranslations": [],
+                "overviewTranslations": [],
+                "lastUpdated": "2022-09-27 22:55:28"
+            }
+            ],
+            "seasonTypes": [
+                {
+                    "id": 1,
+                    "name": "Aired Order",
+                    "type": "official",
+                    "alternateName": "Aired Order"
+                },
+                {
+                    "id": 2,
+                    "name": "DVD Order",
+                    "type": "dvd",
+                    "alternateName": None
+                },
+                {
+                    "id": 3,
+                    "name": "Absolute Order",
+                    "type": "absolute",
+                    "alternateName": None
+                }
+            ]
+        }
+    }
+
+def mock_episode_response(order: str, language: str) -> dict:
+    return {
+        "status": "success",
+        "data": {
+            "id": 78874,
+            "name": f"{language}-Firefly",
+            "slug": "firefly",
+            "firstAired": "2002-09-20",
+            "lastAired": "2003-07-28",
+            "nextAired": "",
+            "score": 156079,
+            "status": {
+                "id": 2,
+                "name": "Ended",
+                "recordType": "series",
+                "keepUpdated": False
+            },
+            "originalCountry": "usa",
+            "originalLanguage": "eng",
+            "defaultSeasonType": 1,
+            "isOrderRandomized": False,
+            "averageRuntime": 48,
+            "episodes": [
+                {
+                    "id": hash(order),
+                    "seriesId": 78874,
+                    "name": f"{order}-{language}-Episode 1",
+                    "aired": "2002-12-20",
+                    "runtime": 87,
+                    "overview": f"Overview in {language}",
+                    "number": 1,
+                    "absoluteNumber": 1,
+                    "seasonNumber": 1
+                },
+            ],
+            "overview": f"Series Overview in {language}",
+            "year": "2002"
+        },
+        "links": {
+            "prev": None,
+            "self": "https://api4.thetvdb.com/v4/series/78874/episodes/dvd/eng?page=0",
+            "next": None,
+            "total_items": 17,
+            "page_size": 500
+        }
+    }
+
+def mock_fetch_series_responses(endpoint: str, *args, **kwargs):
+    if endpoint.endswith("extended"):
+        return mock_series_response()
+    elif "translations" in endpoint:
+        lang = endpoint.split("/")[-1]
+        return {"data": {"name": f"{lang}-Firefly"}}
+    else:
+        order = endpoint.split("/")[-2]
+        lang = endpoint.split("/")[-1]
+        return mock_episode_response(order, lang)
 
 def test_search_by_title_only_query(monkeypatch: MonkeyPatch, provider: TVDBProvider) -> None:
     monkeypatch.setattr(cache, "has", lambda x: False)
@@ -163,3 +331,33 @@ def test_search_by_title_with_invalid_translation(monkeypatch: MonkeyPatch, prov
     ]
 
     assert actual == expected
+
+
+def test_fetch_series_valid_order(monkeypatch: MonkeyPatch, provider: TVDBProvider) -> None:
+    monkeypatch.setattr(provider, "_make_request", mock_fetch_series_responses)
+
+    config = model.SeriesConfig(series_id=78874, order="dvd", language="eng")
+    actual = provider.fetch_series(config)
+
+    expected_config = config
+
+    expected_seasons = []
+    expected = model.Series(
+        tvdb_id=78874,
+        title='eng-Firefly',
+        year='2002',
+        last_aired=datetime.date(2003, 7, 28),
+        retrieved=datetime.date(2026, 6, 8),
+        keep_updated=False,
+    )
+
+
+    assert actual == expected
+    assert actual.config == expected.config
+    assert actual.seasons == expected.seasons
+    for actual_season, expected_season in zip(actual.seasons, expected_seasons):
+        assert actual_season.season_episodes == expected_season.season_episodes
+
+    assert actual.seasons.season_episodes
+
+
